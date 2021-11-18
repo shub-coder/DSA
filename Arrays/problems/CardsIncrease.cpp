c9637918599460992dc65c0194c5272ea961cd4a
shub-coder <53148215+shub-coder@users.noreply.github.com>
2021-11-18 20:56:06 +0530

    Add files via upload

---
+++ Arrays/problems/CardsIncrease.cpp
@@ -0,0 +1,76 @@
+/*You are given an integer array deck. There is a deck of cards where every card has a unique integer. The integer on the ith card is deck[i].
+
+You can order the deck in any order you want. Initially, all the cards start face down (unrevealed) in one deck.
+
+You will do the following steps repeatedly until all cards are revealed:
+
+Take the top card of the deck, reveal it, and take it out of the deck.
+If there are still cards in the deck then put the next top card of the deck at the bottom of the deck.
+If there are still unrevealed cards, go back to step 1. Otherwise, stop.
+Return an ordering of the deck that would reveal the cards in increasing order.
+
+Note that the first entry in the answer is considered to be the top of the deck.
+
+ 
+
+Example 1:
+
+Input: deck = [17,13,11,2,3,5,7]
+Output: [2,13,3,11,5,17,7]
+Explanation: 
+We get the deck in the order [17,13,11,2,3,5,7] (this order does not matter), and reorder it.
+After reordering, the deck starts as [2,13,3,11,5,17,7], where 2 is the top of the deck.
+We reveal 2, and move 13 to the bottom.  The deck is now [3,11,5,17,7,13].
+We reveal 3, and move 11 to the bottom.  The deck is now [5,17,7,13,11].
+We reveal 5, and move 17 to the bottom.  The deck is now [7,13,11,17].
+We reveal 7, and move 13 to the bottom.  The deck is now [11,17,13].
+We reveal 11, and move 17 to the bottom.  The deck is now [13,17].
+We reveal 13, and move 17 to the bottom.  The deck is now [17].
+We reveal 17.
+Since all the cards revealed are in increasing order, the answer is correct.
+JUST SEE THE LOGIC BEHIND THE CODE*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        int m=deck.size();
+        vector<int> ans(m);
+    sort(deck.begin(),deck.end());
+    queue<int> q;
+    for(int i=0;i<m;i++)
+        q.push(i);
+    int i=0;
+    while(!q.empty()){
+        int x=q.front();
+        ans[x]=deck[i++];
+        q.pop();
+        if(q.empty())
+        {
+            for(int i=0; i<ans.size(); i++)
+            {
+                cout<<ans[x];
+            }
+        }    
+        x=q.front();
+        q.pop();
+        q.push(x);
+    }
+    for(auto x: ans)
+    {
+        cout<<ans[x]<<endl;
+    }
+}
+
+    int main()
+    {
+        int n;
+        cin>>n;
+       vector<int>deck(n);
+       for(int i=0; i<n; i++)
+       {
+           cin>>deck[i];
+       }
+       deckRevealedIncreasing(deck);
+
+    }
